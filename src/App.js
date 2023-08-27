c38c6aa7152d0fafd70b2d916dfc33dbe738b088
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-08-28 02:31:26 +0600

    (advanced) Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 // const number = 343;
-// const singer = { name: "Asif", job: "singer-1" };
-// const singer2 = { name: "Eva Rahman", job: "singer-2" };
+const singers = [
+  { name: "Asif", job: "singer-1" },
+  { name: "Eva Rahman", job: "singer-2" },
+  { name: "Agun", job: "sopno" },
+  { name: "Suvro", job: "pathor" },
+];
 // const style = {
 //   color: "yellow",
 //   backgroundColor: "purple",
@@ -12,34 +16,55 @@ import "./App.css";
 // };
 
 function App() {
+  const nayoks = [
+    "Rubel",
+    "Joshim",
+    "Manna",
+    "BappaRaj",
+    "Salmanship",
+    "Shakib Khan",
+    "Siam",
+  ];
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      {nayoks.map((nayok) => (
+        <li>Name:{nayok}</li>
+      ))}
+
+      {/* {nayoks.map((nayok) => (
+        <Person name={nayok}></Person>
+      ))} */}
+
+      {singers.map((singer) => (
+        <Person name={singer.name}></Person>
+      ))}
+      {/* <Person name={nayoks[0]} naika="Mouri"></Person>
+      <Person name={nayoks[1]} naika="Sabnur"></Person>
+      <Person></Person> */}
       <h5>New Component. YAY</h5>
       <p>Rock Mama. React Mama</p>
-      <Friend></Friend>
-      <Friend></Friend>
+      <Friend name="Ajoy Devgun" phone="0128456"></Friend>
+      <Friend name="King Khan" phone="01922940626"></Friend>
     </div>
   );
 }
-<Person></Person>;
 
-function Person() {
+function Person(props) {
+  // console.log(props);
   return (
     <div className="person">
-      <h1>Shakib Al Hasan</h1>
-      <p>Profession: Cricketer</p>
+      <h1>Nayok: {props.name}</h1>
+      <p>Naika: {props.naika}</p>
     </div>
   );
 }
 
-function Friend() {
+function Friend(props) {
+  // console.log(props);
   return (
     <div className="container">
-      <h3>Name: Ajay Devgun</h3>
-      <p>Profession: Acting</p>
+      <h3>Name: {props.name}</h3>
+      <p>Phone: {props.phone} </p>
     </div>
   );
 }
